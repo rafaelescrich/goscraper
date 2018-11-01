a2fc2b5c2a8a7381916f21abe9735b3a3d686f82
Rafael Escrich <rafaelescrich@gmail.com>
2018-11-01 12:34:34 -0300

    Test pipeline example

--- main.go
+++ main.go
@@ -1,36 +1,135 @@
+// package main
+
+// import (
+// 	"fmt"
+// 	"net/http"
+
+// 	"github.com/yhat/scrape"
+// 	"golang.org/x/net/html"
+// 	"golang.org/x/net/html/atom"
+// )
+
+// func main() {
+// 	// request and parse the front page
+// 	resp, err := http.Get("https://news.ycombinator.com/")
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	root, err := html.Parse(resp.Body)
+// 	if err != nil {
+// 		panic(err)
+// 	}
+
+// 	// define a matcher
+// 	matcher := func(n *html.Node) bool {
+// 		// must check for nil values
+// 		if n.DataAtom == atom.A && n.Parent != nil && n.Parent.Parent != nil {
+// 			return scrape.Attr(n.Parent.Parent, "class") == "athing"
+// 		}
+// 		return false
+// 	}
+// 	// grab all articles and print them
+// 	articles := scrape.FindAll(root, matcher)
+// 	for i, article := range articles {
+// 		fmt.Printf("%2d %s (%s)\n", i, scrape.Text(article), scrape.Attr(article, "href"))
+// 	}
+// }
+
 package main
 
 import (
 	"fmt"
 	"net/http"
+	"sync"
 
 	"github.com/yhat/scrape"
 	"golang.org/x/net/html"
 	"golang.org/x/net/html/atom"
 )
 
-func main() {
-	// request and parse the front page
-	resp, err := http.Get("https://news.ycombinator.com/")
-	if err != nil {
-		panic(err)
-	}
-	root, err := html.Parse(resp.Body)
-	if err != nil {
-		panic(err)
-	}
+// Set your email here to include in the User-Agent string.
+var email = "youremail@gmail.com"
+var urls = []string{
+	"http://techcrunch.com/",
+	"https://www.reddit.com/",
+	"https://en.wikipedia.org",
+	"https://news.ycombinator.com/",
+	"https://www.buzzfeed.com/",
+	"http://digg.com",
+}
 
-	// define a matcher
-	matcher := func(n *html.Node) bool {
-		// must check for nil values
-		if n.DataAtom == atom.A && n.Parent != nil && n.Parent.Parent != nil {
-			return scrape.Attr(n.Parent.Parent, "class") == "athing"
-		}
-		return false
+func respGen(urls ...string) <-chan *http.Response {
+	var wg sync.WaitGroup
+	out := make(chan *http.Response)
+	wg.Add(len(urls))
+	for _, url := range urls {
+		go func(url string) {
+			req, err := http.NewRequest("GET", url, nil)
+			if err != nil {
+				panic(err)
+			}
+			req.Header.Set("user-agent", "testBot("+email+")")
+			resp, err := http.DefaultClient.Do(req)
+			if err != nil {
+				panic(err)
+			}
+			out <- resp
+			wg.Done()
+		}(url)
 	}
-	// grab all articles and print them
-	articles := scrape.FindAll(root, matcher)
-	for i, article := range articles {
-		fmt.Printf("%2d %s (%s)\n", i, scrape.Text(article), scrape.Attr(article, "href"))
+	go func() {
+		wg.Wait()
+		close(out)
+	}()
+	return out
+}
+
+func rootGen(in <-chan *http.Response) <-chan *html.Node {
+	var wg sync.WaitGroup
+	out := make(chan *html.Node)
+	for resp := range in {
+		wg.Add(1)
+		go func(resp *http.Response) {
+			root, err := html.Parse(resp.Body)
+			if err != nil {
+				panic(err)
+			}
+			out <- root
+			wg.Done()
+		}(resp)
+	}
+	go func() {
+		wg.Wait()
+		close(out)
+	}()
+	return out
+}
+
+func titleGen(in <-chan *html.Node) <-chan string {
+	var wg sync.WaitGroup
+	out := make(chan string)
+	for root := range in {
+		wg.Add(1)
+		go func(root *html.Node) {
+			title, ok := scrape.Find(root, scrape.ByTag(atom.Title))
+			if ok {
+				out <- scrape.Text(title)
+			}
+			wg.Done()
+		}(root)
+	}
+	go func() {
+		wg.Wait()
+		close(out)
+	}()
+	return out
+}
+
+func main() {
+	// Set up the pipeline to consume back-to-back output
+	// ending with the final stage to print the title of
+	// each web page in the main go routine.
+	for title := range titleGen(rootGen(respGen(urls...))) {
+		fmt.Println(title)
 	}
 }
